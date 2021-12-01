bf9f9fba8e2ab3dab546cba246943f3367480c2f
temporini <91788264+temporini@users.noreply.github.com>
2021-12-01 13:46:58 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
 
+int nprimo(int a, int b) {
+    int risultato = 1, n = 0;
+
+    if (b > 1 && a > 1) {
+
+        risultato = nprimo(a - 1, b);
+
+        n = b % a;
+
+        if (n == 0) {
+            return 0;
+        }
+
+    }
+    return risultato;
+}
